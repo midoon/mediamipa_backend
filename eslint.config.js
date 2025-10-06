d70069af1e697781755845972842b9ee36e48d98
midoon <harunkoding@gmail.com>
2025-10-06 13:21:43 +0700

    eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,31 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import { defineConfig } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  js.configs.recommended, // dari @eslint/js
+  ...tseslint.configs.recommended, // dari typescript-eslint
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    ignores: ["node_modules", "dist", "build", "coverage", "src/generated"],
-    plugins: {
-      js,
-      prettier: eslintPluginPrettier,
-    },
-    extends: [
-      "js/recommended",
-      ...tseslint.configs.recommended,
-      "plugin:prettier/recommended", // <= ini integrasi utama
-    ],
     languageOptions: {
-      globals: globals.node, // gunakan node untuk backend
+      globals: globals.node,
+    },
+    plugins: {
+      prettier: prettierPlugin,
     },
     rules: {
-      "prettier/prettier": ["error"],
+      ...eslintConfigPrettier.rules, // disable rules yg bentrok dgn prettier
+      "prettier/prettier": "error", // aktifkan prettier sebagai rule
     },
   },
+  globalIgnores( [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/coverage/**",
+      "**/src/generated/**", // <--- pastikan ada trailing /** agar semua file di-skip
+    ]),
 ]);
